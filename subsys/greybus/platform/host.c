a4ca0d1f907109ee727764989194299b7a903964
Yacin Belmihoub-Martel <yacin.belmihoub-martel@silabs.com>
2026-03-16 14:15:52 -0400

    add host hdlc transport supprot

--- subsys/greybus/platform/host.c
+++ subsys/greybus/platform/host.c
@@ -6,11 +6,24 @@
 
 #include <zephyr/init.h>
 #include <zephyr/logging/log.h>
+#include <greybus/greybus.h>
+#include "../greybus_internal.h"
+#include "../greybus_transport.h"
 
 LOG_MODULE_REGISTER(greybus_host, CONFIG_GREYBUS_LOG_LEVEL);
 
 int greybus_host_init(void)
 {
+	const struct gb_transport_backend *xport = gb_transport_get_backend();
+
+	LOG_DBG("Greybus initializing..");
+
+	int r = gb_init(xport);
+	if (r < 0) {
+		LOG_ERR("gb_init() failed: %d", r);
+		return r;
+	}
+
 	LOG_INF("Greybus host is active");
 	return 0;
 }
